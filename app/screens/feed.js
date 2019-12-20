3895e852f1a52225902905a2c3c9b1299aaae4d9
Joseph <joejoshw@gmail.com>
2019-12-20 08:19:38 +0300

    Added flatlist to the feed screen, with pull to refresh
    Signed-off-by: Joseph <joejoshw@gmail.com>

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -2,10 +2,25 @@ import React from 'react';
 import {
     FtatList, StyleSheet, Text, View, Image
  } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
  class feed extends React.Component {
      constructor(props) {
          super(props);
+         this.state = {
+             photo_feed: [0,1,2,3,4],
+             refresh: false
+         }
+     }
+
+     loadNew = () => {
+         this.setState({
+             refresh: true
+         });
+         this.setState({
+             photo_feed: [5,6,7,8,9],
+             refresh: false
+         });
      }
 
      render() {
@@ -14,22 +29,30 @@ import {
              <View style={{height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightGrey', borderBottomWidth: 0.5, justifyContent: "center", alignItems: "center"}}>
                 <Text>Feed</Text>
              </View>
-             
-             <View>
-                <View>
-                    <Text>Time Ago</Text>
-                    <Text>@Rusty</Text>
-                </View>
-                <View>
-                    <Image source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor((Math.random() * 800) + 500)}}
-                    style={{resizeMode: 'cover', width: '100%', height: 275}}
-                    />                    
-                </View>
-                <View>
-                    <Text>Caption test here...</Text>
-                    <Text>View Comments</Text>
-                </View>
-             </View>
+             <FlatList
+                refreshing={this.state.refresh}
+                onRefresh={() => this.loadNew}
+                data={this.state.photo_feed}
+                keyExtractor={(item, index) => index.toString()}
+                style={{flex:1, backgroundColor:'#eee'}}
+                renderItem={({item, index}) => (
+                    <View key={index}>
+                        <View>
+                            <Text>Time Ago</Text>
+                            <Text>@Rusty</Text>
+                        </View>
+                        <View>
+                            <Image source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor((Math.random() * 800) + 500)}}
+                            style={{resizeMode: 'cover', width: '100%', height: 275}}
+                            />                    
+                        </View>
+                        <View>
+                            <Text>Caption test here...</Text>
+                            <Text>View Comments</Text>
+                        </View>
+                    </View>
+                )}
+             />
 
              </View>
          )
