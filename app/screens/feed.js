5f8b3209a8df670450cf8cd5bee84105f199f5b6
Joseph <joejoshw@gmail.com>
2019-12-20 08:05:27 +0300

    Add basic image and texts to the feed screen
    Signed-off-by: Joseph <joejoshw@gmail.com>

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -10,8 +10,27 @@ import {
 
      render() {
          return ( 
-             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-             <Text>Feed</Text>
+             <View style={{flex: 1}}>
+             <View style={{height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightGrey', borderBottomWidth: 0.5, justifyContent: "center", alignItems: "center"}}>
+                <Text>Feed</Text>
+             </View>
+             
+             <View>
+                <View>
+                    <Text>Time Ago</Text>
+                    <Text>@Rusty</Text>
+                </View>
+                <View>
+                    <Image source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor((Math.random() * 800) + 500)}}
+                    style={{resizeMode: 'cover', width: '100%', height: 275}}
+                    />                    
+                </View>
+                <View>
+                    <Text>Caption test here...</Text>
+                    <Text>View Comments</Text>
+                </View>
+             </View>
+
              </View>
          )
      }
